d1922dacbc270dbc83c29223808a97bc57391d52
MclPio <113801014+MclPio@users.noreply.github.com>
2025-03-14 17:54:32 -0400

    Adjust routes and home page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "projects#index"
+  root "pages#home"
   get "/home", to: "pages#home"
   resource :registration, only: %i[new create]
   resources :projects, only: %i[new create show index] do
