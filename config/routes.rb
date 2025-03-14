7a28482c0ddb6235be27f9c6c4fd57b97485126e
MclPio <113801014+MclPio@users.noreply.github.com>
2025-03-14 18:10:53 -0400

    add project index route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   root "projects#index"
   get "/home", to: "pages#home"
   resource :registration, only: %i[new create]
-  resources :projects, only: %i[new create show] do
+  resources :projects, only: %i[new create show index] do
     member do
       patch :update_idea
     end
