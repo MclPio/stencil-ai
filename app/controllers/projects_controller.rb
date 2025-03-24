fb033d3d4df781a1ba866f123232fcd4c3a8381c
MclPio <113801014+MclPio@users.noreply.github.com>
2025-03-23 20:51:19 -0400

    Modify create project redirect path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Current.user.projects.new(project_params)
     if @project.save
-      redirect_to project_path(@project)
+      redirect_to project_conversation_path(@project)
     else
       render :new, status: :unprocessable_entity
     end
