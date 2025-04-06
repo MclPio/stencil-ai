39da93067c39180b8ffd8192877b7191a5f5f45b
MclPio <113801014+MclPio@users.noreply.github.com>
2025-04-06 15:56:21 -0400

    Add half panel length for artifacts

--- app/javascript/controllers/artifact_controller.js
+++ app/javascript/controllers/artifact_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import mermaid from "mermaid";
 
 // Connects to data-controller="artifact"
 export default class extends Controller {
-  static targets = [ "panel" ]
+  static targets = [ "panel", "divider" ]
+
+  connect() {
+    mermaid.initialize({
+      startOnLoad: false,  // Don't auto-render on page load
+      theme: "default",
+    });
+  }
 
   toggle() {
     this.panelTarget.classList.toggle("hidden")
+    this.dividerTarget.classList.toggle("hidden")
   }
 }
