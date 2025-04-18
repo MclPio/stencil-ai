1e2b8d2f83c57f7632f055e81d2e67ee35092bb2
MclPio <113801014+MclPio@users.noreply.github.com>
2025-04-18 15:02:32 -0400

    Add LoadChartOnOpen

--- app/javascript/controllers/artifact_controller.js
+++ app/javascript/controllers/artifact_controller.js
@@ -1,8 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
+import mermaid from "mermaid";
 
 // Connects to data-controller="artifact"
 export default class extends Controller {
-  static targets = [ "panel", "divider", "openButton" ]
+  static targets = [ "panel", "divider", "openButton", "selection", "artifactContent" ]
+  static values = { projectId: Number }
 
   close() {
     this.panelTarget.classList.add("hidden");
@@ -14,5 +16,25 @@ export default class extends Controller {
     this.panelTarget.classList.remove("hidden");
     this.dividerTarget.classList.remove("hidden");
     this.openButtonTarget.classList.add("hidden");
+    this.loadChartOnOpen()
+  }
+
+  loadChartOnOpen() {
+    const url = `/projects/${this.projectIdValue}/artifacts/${this.selectionTarget.value}`
+    this.load(url)
+  }
+
+  load(urlValue) {
+    fetch(urlValue)
+      .then(response => response.text())
+      .then(html => {
+        this.artifactContentTarget.innerHTML = html;
+        mermaid.run({
+          querySelector: '.mermaid',
+          suppressErrors: false
+        }).catch(err => {
+          console.error('Mermaid rendering error:', err);
+        });
+      })
   }
 }
