f3ffa9eeb86c671105c5e5e02ffc2cb3b77bc8e6
MclPio <113801014+MclPio@users.noreply.github.com>
2025-05-22 18:39:21 -0400

    Remove locals for button update

--- app/jobs/generate_erd_diagram_job.rb
+++ app/jobs/generate_erd_diagram_job.rb
@@ -30,7 +30,7 @@ class GenerateErdDiagramJob < ApplicationJob
                     <path stroke-linecap='round' stroke-linejoin='round' d='M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18' />
                   </svg>
               </button>",
-        locals: { message: assistant_message }
+        locals: { }
       )
   end
 end
