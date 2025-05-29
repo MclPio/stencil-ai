242763f13be377efd39239e1bd8d7b5cd3ea1aa5
MclPio <113801014+MclPio@users.noreply.github.com>
2025-05-29 19:35:32 -0400

    Add token limits to user types

--- app/models/weekly_consumption.rb
+++ app/models/weekly_consumption.rb
@@ -2,4 +2,8 @@ class WeeklyConsumption < ApplicationRecord
   belongs_to :user
 
   validates :credits, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  FREE_TOKEN_LIMIT = 25_000
+  SUBSCRIBER_TOKEN_LIMIT = 100_000_000
+  ADMIN_TOKEN_LIMIT = 100_000_000
 end
