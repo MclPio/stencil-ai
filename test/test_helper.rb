b456d332b1f95b8975b143f1e93122ac9832df45
mcl <113801014+MclPio@users.noreply.github.com>
2025-06-01 14:41:26 -0400

    Update test_helper.rb remove shoulda

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,9 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require 'shoulda/matchers' # Require it early
 
 module ActiveSupport
   class TestCase
-    # Configure Shoulda Matchers directly within ActiveSupport::TestCase
-    # to ensure it's applied to this class and its descendants.
-    Shoulda::Matchers.configure do |config|
-      config.integrate do |with|
-        with.test_framework :minitest
-        with.library :rails
-      end
-    end
 
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
