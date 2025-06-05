1b61361d7c9a0d14574a093bf7d183928688d211
MclPio <113801014+MclPio@users.noreply.github.com>
2025-06-05 16:48:06 -0400

    Add user artifact stencil collection to favorite artifact stencils controller

--- app/controllers/favorite_artifact_stencils_controller.rb
+++ app/controllers/favorite_artifact_stencils_controller.rb
@@ -6,6 +6,10 @@ class FavoriteArtifactStencilsController < ApplicationController
     @favorite_artifact_stencils = FavoriteArtifactStencil.where(user: Current.user).includes(:artifact_stencil).map do |fas|
       { id: fas.artifact_stencil.id, name: fas.artifact_stencil.name, description: fas.artifact_stencil.description, published: fas.artifact_stencil.published, user: fas.artifact_stencil.user.name }
     end
+
+    @user_artifact_stencils = Current.user.artifact_stencils.map do |i|
+      { id: i.id, name: i.name, description: i.description, published: i.published, user: i.user.name }
+    end
   end
 
   def create
