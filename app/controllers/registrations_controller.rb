04ccbc78f8982b53977f294fb8c881c303b4c6d4
MclPio <113801014+MclPio@users.noreply.github.com>
2025-06-13 11:01:36 -0400

    Add destroy action to registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,12 @@
 class RegistrationsController < ApplicationController
-  allow_unauthenticated_access
+  allow_unauthenticated_access only: %i[new create]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_registration_url, alert: "Try again later." }
 
   def new
     @user = User.new
+  end
+
+  def show
   end
 
   def create
@@ -10,6 +14,15 @@ class RegistrationsController < ApplicationController
     if @user.save
       start_new_session_for @user
       redirect_to root_path, notice: "Successfully signed up!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if Current.user.destroy
+      terminate_session
+      redirect_to new_session_path, notice: "Your account has been successfully deleted."
     else
       render :new, status: :unprocessable_entity
     end
