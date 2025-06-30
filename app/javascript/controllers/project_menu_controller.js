1d76e83e74ae1479f1002b4b17ee99a7833bc860
MclPio <113801014+MclPio@users.noreply.github.com>
2025-06-30 17:04:27 -0400

    Add link to stencils in chat

--- app/javascript/controllers/project_menu_controller.js
+++ app/javascript/controllers/project_menu_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="project-menu"
 export default class extends Controller {
-  static targets = ["arrow", "newProjectSpan", "projectsSpan", "link" ]
+  static targets = ["arrow", "newProjectSpan", "projectsSpan", "link", "stencilsSpan" ]
 
   expand() {
     const [firstSvg, secondSvg] = this.arrowTarget.querySelectorAll("svg");
@@ -12,6 +12,7 @@ export default class extends Controller {
       secondSvg.classList.add("hidden");
       this.newProjectSpanTarget.classList.add("hidden")
       this.projectsSpanTarget.classList.add("hidden")
+      this.stencilsSpanTarget.classList.add("hidden")
       this.linkTargets.forEach(target => {
         target.classList.add("hidden")
       })
@@ -21,6 +22,7 @@ export default class extends Controller {
       secondSvg.classList.remove("hidden");
       this.newProjectSpanTarget.classList.remove("hidden")
       this.projectsSpanTarget.classList.remove("hidden")
+      this.stencilsSpanTarget.classList.remove("hidden")
       this.linkTargets.forEach(target => {
         target.classList.remove("hidden")
       })
